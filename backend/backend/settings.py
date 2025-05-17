76cff1630e742a630c5c973c4a14111ccdaeba2b
Ivan Rono <princeivan733@gmail.com>
2025-05-17 20:53:30 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['139.162.175.68','ausacreative.co.ke']
 
