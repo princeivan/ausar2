90effd77964b07fc67304d3c4fa70e6fb1b72214
Ivan Rono <princeivan733@gmail.com>
2025-08-05 14:00:30 +0300

    Updating cookies

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -55,8 +55,8 @@ SIMPLE_JWT = {
     "AUTH_COOKIE": "access",
     "AUTH_COOKIE_REFRESH": "refresh",
     "AUTH_COOKIE_HTTP_ONLY": True,
-    "AUTH_COOKIE_SECURE":False,
-    "AUTH_COOKIE_SAMESITE": "Lax",
+    "AUTH_COOKIE_SECURE":True,
+    "AUTH_COOKIE_SAMESITE": None,
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
  }
@@ -190,11 +190,11 @@ CORS_ALLOWED_ORIGINS=[
     'https://ausar2-git-main-ivan-ronos-projects.vercel.app'
 ]
 CORS_ALLOW_CREDENTIALS = True
-CSRF_TRUSTED_ORIGINS = ["http://localhost:5173"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:5173",'https://ausar2-git-main-ivan-ronos-projects.vercel.app']
 SESSION_COOKIE_SAMESITE = "Lax"
 CSRF_COOKIE_SAMESITE = "Lax"
-CSRF_COOKIE_SECURE = False  # True in production
-SESSION_COOKIE_SECURE = False  # True in production
+CSRF_COOKIE_SECURE = True  # True in production
+SESSION_COOKIE_SECURE = True  # True in production
 # CORS_ALLOW_HEADERS = list(default_headers) + [
 #     'authorization',
 # ]
